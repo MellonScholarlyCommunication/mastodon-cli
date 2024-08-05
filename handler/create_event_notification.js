e5f403f8ef11f820d80f4bfe13fd4419948c450f
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-08-05 11:04:25 +0200

    Event notifications support

---
+++ handler/create_event_notification.js
@@ -0,0 +1,100 @@
+const jsdom = require('jsdom');
+const { v4: uuidv4 } = require('uuid');
+const log4js = require('log4js');
+const logger = log4js.getLogger();
+
+/**
+ * Extract references from the toot (if available) and
+ * generate an Event Notification
+ */
+async function handle(item) {
+    const content = item['status']?.['content'];
+
+    if (! content) {
+        logger.error('no content in notification');
+        return [];
+    }
+
+    const dom = new jsdom.JSDOM(content);
+    const jquery = require("jquery")(dom.window);
+
+    const anchors = jquery('a');
+
+    const zreferences = [];
+    
+    for (let i = 0 ; i < anchors.length ; i++) {
+        const href = anchors[i].href;
+        const className = anchors[i].className;
+
+        if ( ! (className.match(/mention/g))) {
+            zreferences.push(href);            
+        }
+    }
+
+    if (! zreferences.length) {
+        logger.warn('no references in notification');
+        return [];
+    }
+
+    const profile = await findProfile(item['account']['url']);
+
+    if (! profile) {
+        logger.warn(`unable to dereference profile ${item['account']['url']}`);
+        return [];
+    }
+
+    const links = [];
+
+    for (let i = 0 ; i < zreferences.length ; i++) {
+        links.push({
+            "type": "Link" ,
+            "href": zreferences[i]
+        });
+    }
+
+    const resultItem = {
+        "@context" : "https://www.w3.org/ns/activitystreams" ,
+        "id": `urn:uuid:${uuidv4()}`,
+        "type": "Announce",
+        "published": item['status']['created_at'],
+        "actor": {
+            "id": item['account']['url'],
+            "name": item['account']['display_name'],
+            "inbox": profile['inbox'],
+            "type": "Person"
+        }, 
+        "origin": {
+            "id": process.env.MASTODON_ORIGIN_ID ,
+            "name": process.env.MASTODON_ORIGIN_NAME ,
+            "inbox": process.env.MASTODON_ORIGIN_INBOX ,
+            "type": process.env.MASTODON_ORIGIN_TYPE
+        },
+        "object": {
+            "id": item['status']['url'],
+            "content": item['status']['content'],
+            "url": links ,
+            "type": "Note"
+        }
+    };
+
+    return [ resultItem ];
+}
+
+async function findProfile(url) {
+    logger.debug(`dereferencing ${url}`);
+    const response = await fetch(url, {
+        headers: {
+            'accept': 'application/activity+json'
+        }
+    });
+
+    if (response.ok) {
+        return await response.json();
+    }
+    else {
+        logger.error(`got ${response.status} - ${response.statusText} when dereferencing ${url}`);
+        return undefined;
+    }
+}
+
+module.exports = { handle };
